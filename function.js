11c86f55f45dea77633af7ad2a70736f7645c281
Irandi Silva <167270911+Irandisilvaa@users.noreply.github.com>
2025-04-08 08:50:03 -0300

    Update function.js

--- function.js
+++ function.js
@@ -289,7 +289,7 @@ gerarPdf.addEventListener('click', async () => {
  doc.text(`${bairro}`, 50, 102);
  doc.text(`${telefone1Formatado}`, 51, 112);
  doc.text(`${telefone2Formatado}`, 155, 112);
- doc.text(`${nomeAtendenteSpan}`, 163, 43);
+ doc.text(`${nomeAtendenteParaCapa}`, 163, 38);
 
    if (sexo === 'masculino') {
      doc.text('X', 162, 60); // Posição para masculino
